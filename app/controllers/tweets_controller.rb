4800d15739f17de104572d04ab4b29a0235b0a8d
David BF <burretfresket@gmail.com>
2015-11-13 22:53:25 +0100

    Show user Twitter timeline

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,23 @@
 class TweetsController < ApplicationController
   def index
-    return redirect_to logout_path unless current_user
+    return render '_homepage' unless current_user
+
+    user = tw_client.user
+    @name = user.name
+    @avatar_url = user.profile_image_url
+    @tweets_count = user.tweets_count
+    @tweets = tw_client.user_timeline
+  end
+
+  private
+
+  def tw_client
+    secrets = Rails.application.secrets
+    @tw_client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = secrets[:tw_consumer_key]
+      config.consumer_secret = secrets[:tw_consumer_secret]
+      config.access_token = current_user['token']
+      config.access_token_secret = current_user['secret']
+    end
   end
 end
